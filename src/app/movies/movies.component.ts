c7c46366aecf7cf9200dd4f8c5fb257506745c36
Almir <almir.tihak98@hotmail.com>
2021-06-14 23:03:12 +0200

    finished movies

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -9,6 +9,8 @@ import { MovieService } from '../_services/movie.service';
 export class MoviesComponent implements OnInit {
 
   movies: any;
+  currentPage = 1;
+  backgroundImage: string;
 
   constructor(private movieService: MovieService) { }
 
@@ -18,7 +20,7 @@ export class MoviesComponent implements OnInit {
 
   getAllMovies()
   {
-    this.movieService.getPopular('movie', 1).subscribe(response =>{
+    this.movieService.getPopular('movie', this.currentPage).subscribe(response =>{
 
       this.movies = response;
     }, error =>{
@@ -26,4 +28,41 @@ export class MoviesComponent implements OnInit {
     });
   }
 
+  onMouseOver(movie) {
+    this.backgroundImage = movie.backdrop_path;
+  }
+
+  scrollToTop()
+  {
+    const el: any | null = document.getElementById('main');
+    window.scrollTo({
+      top: el.offsetTop - 60,
+      behavior: 'smooth',
+    });
+  }
+
+  LoadNewMovies()
+  {
+    this.currentPage++;
+    this.movieService.getPopular('movie', this.currentPage).subscribe(response =>{
+
+      this.movies = response;
+      this.scrollToTop();
+    }, error =>{
+      console.log(error);
+    });
+  }
+
+  LoadPrevMovies()
+  {
+    this.currentPage--;
+    console.log(this.currentPage);
+    this.movieService.getPopular('movie', this.currentPage).subscribe(response =>{
+
+      this.movies = response;
+      this.scrollToTop();
+    }, error =>{
+      console.log(error);
+    });
+  }
 }
